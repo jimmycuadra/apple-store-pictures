ee7c7cae7b6d542b9c2f7d54f8e843cf843369f6
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-08-27 23:42:35 -0700

    now you're cooking with fruit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source :rubygems
+
+# rails
+gem 'rails', '3.1.0.rc6'
+
+# server
+gem 'thin'
+
+# assets
+gem 'sass-rails', '~> 3.1.0.rc'
+gem 'uglifier'
+
+# javascript
+gem 'jquery-rails'
+
+gem 'pg', :group => [:production]
+gem 'sqlite3', :group => [:development, :test]
+
+# testing
+group :development, :test do
+  gem 'rspec-rails', '~> 2.6.1'
+  gem 'guard-rspec'
+  gem 'rb-fsevent'
+  gem 'growl'
+  gem 'simplecov'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppleStorePictures::Application.config.secret_token = 'bc3e43fc30254485730a9ab44b7375f14420446c03c34de20915dd44b7f4bf190b21803b2bd227027e02b42df702049710393fe78f8417c607056c6e9cdab5e4'
