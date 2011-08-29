928b5b15fb17e04e699e7ab7985239a7f1cb420c
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-08-29 01:09:44 -0700

    add image/form to infoWindow

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,6 +3,7 @@ class Store < ActiveRecord::Base
 
   def as_json(options = {})
     {
+      :id => self.id,
       :name => self.name,
       :address => self.address,
       :latitude => self.latitude,
